96a9e943aa788c37acf698c52adf3c94d62c075e
unknown <ehsanullahhaidary@gmail.com>
2023-08-28 21:46:35 +0430

    Navbar js and css has been added to project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Buttons from "./components/Buttons";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Buttons />
+      <Navbar />
     </div>
   );
 }
