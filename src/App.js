6a82f09c2e58078013ffa614b1b2fd69f3015e6f
unknown <ehsanullahhaidary@gmail.com>
2023-09-08 19:23:22 +0430

    React-Router-Dom added to project
    in app.js routes added and setuped

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import "./App.css";
 import Header from "./components/Header";
 import Buttons from "./components/Buttons";
 import Navbar from "./components/Navbar";
-import Vdeos from "./components/Videos";
+import Videos from "./components/Videos";
 import BottomNav from "./components/BottomNav";
+import { Routes, Route } from "react-router-dom";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Navbar />
+      <BottomNav />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Buttons />
+              <Videos />
+            </div>
+          }
+        />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+      {/* <Header />
       <Buttons />
       <Navbar />
-      <Vdeos />
-      <BottomNav />
-      <h1>hello</h1>
+      <Videos />
+      <BottomNav /> */}
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Search.css";
+
+function Search() {
+  return (
+    <div className="searchVideos">
+      <div>Search</div>
+    </div>
+  );
+}
+
+export default Search;
