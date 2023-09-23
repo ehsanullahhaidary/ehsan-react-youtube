5219c43ecc0d48ddc7285efc8d5bb972b5ded33c
unknown <ehsanullahhaidary@gmail.com>
2023-09-23 21:51:39 +0430

    Got data from API for search component and setuped the navigation to search component and getting data from input search and  passing it to search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import "./Search.css";
+// import useLocation from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { getVideosByKeyword } from "./VideosAPIHandler";
 
 function Search() {
+  const location = useLocation();
+  const {
+    state: { inputSearch },
+  } = location;
+  const [data, setData] = useState([]);
+  const [nextPageToken, setNextPageToken] = useState("");
+
+  useEffect(() => {
+    try {
+      getVideosByKeyword(inputSearch).then((res) => {
+        setData(res.data.items);
+        setNextPageToken(res.data.nextPageToken);
+        console.log(res.data.items);
+      });
+    } catch (error) {}
+  }, [inputSearch]);
   return (
     <div className="searchVideos">
       <div>Search</div>
